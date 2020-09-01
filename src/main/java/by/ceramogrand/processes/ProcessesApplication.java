a92fee453d52b89bd7dbe831795605100abdd79e
ushenko-dmitry <dimaushenko@mail.ru>
2020-09-01 19:18:52 +0300

    init

---
+++ src/main/java/by/ceramogrand/processes/ProcessesApplication.java
@@ -0,0 +1,13 @@
+package by.ceramogrand.processes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/ceramogrand/processes/ProcessesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ceramogrand.processes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
