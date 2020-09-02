3c63464845504df4a4485650aa933b0b3263e6f9
ushenko-dmitry <dimaushenko@mail.ru>
2020-09-02 20:43:21 +0300

    add find user by email

--- repository-module/src/main/java/by/ceramogrand/processes/repository/UserRepository.java
+++ repository-module/src/main/java/by/ceramogrand/processes/repository/UserRepository.java
@@ -4,4 +4,6 @@ import by.ceramogrand.processes.repository.model.User;
 
 public interface UserRepository extends GenericRepository<Long, User> {
 
+    User getUserByEmail(String email);
+
 }
--- repository-module/src/main/java/by/ceramogrand/processes/repository/impl/UserRepositoryImpl.java
+++ repository-module/src/main/java/by/ceramogrand/processes/repository/impl/UserRepositoryImpl.java
@@ -4,7 +4,17 @@ import by.ceramogrand.processes.repository.UserRepository;
 import by.ceramogrand.processes.repository.model.User;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
+
 @Repository
 public class UserRepositoryImpl extends GenericRepositoryImpl<Long, User> implements UserRepository {
 
+    @Override
+    public User getUserByEmail(String email) {
+        String queryString = "FROM " + entityClass.getSimpleName() + " u "
+                + "WHERE u.email=:email";
+        Query query = entityManager.createQuery(queryString);
+        query.setParameter("email", email);
+        return (User) query.getSingleResult();
+    }
 }
