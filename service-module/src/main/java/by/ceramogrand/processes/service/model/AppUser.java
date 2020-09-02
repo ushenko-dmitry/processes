25dc3a1613bd6cb0bfb747ec3dd8ef0081ec0233
ushenko-dmitry <dimaushenko@mail.ru>
2020-09-02 21:02:50 +0300

    add basic auth, some changes

---
+++ service-module/src/main/java/by/ceramogrand/processes/service/model/AppUser.java
@@ -0,0 +1,61 @@
+package by.ceramogrand.processes.service.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class AppUser implements UserDetails {
+
+    private final UserDTO userDTO;
+    private final List<SimpleGrantedAuthority> authorities;
+
+    public AppUser(UserDTO userDTO) {
+        this.userDTO = userDTO;
+        this.authorities = Collections.singletonList(
+                new SimpleGrantedAuthority(
+//                        "ROLE_" + userDTO.getRole().name()
+                        "ROLE_ADMIN"
+                )
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return userDTO.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userDTO.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
