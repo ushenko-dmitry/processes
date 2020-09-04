47dfd3bc51e543e538ef5d6370ab790e5d6684ee
ushenko-dmitry <dimaushenko@mail.ru>
2020-09-04 21:51:25 +0300

    init taskPattern

---
+++ repository-module/src/main/java/by/ceramogrand/processes/repository/model/TaskPattern.java
@@ -0,0 +1,130 @@
+package by.ceramogrand.processes.repository.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private String description;
+
+    @Column
+    private Boolean complete;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "completed_by", nullable = true)
+    private User completedBy;
+    @Column(name = "date_completed", nullable = true)
+    private LocalDateTime dateCompleted;
+
+    @Column
+    private Boolean deleted;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "created_by")
+    private User createdBy;
+    @Column(name = "date_created")
+    private LocalDate dateCreated;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "updated_by", nullable = true)
+    private User updatedBy;
+    @Column(name = "date_updated", nullable = true)
+    private LocalDate dateUpdated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getComplete() {
+        return complete;
+    }
+
+    public void setComplete(Boolean complete) {
+        this.complete = complete;
+    }
+
+    public User getCompletedBy() {
+        return completedBy;
+    }
+
+    public void setCompletedBy(User completedBy) {
+        this.completedBy = completedBy;
+    }
+
+    public LocalDateTime getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public void setDateCompleted(LocalDateTime dateCompleted) {
+        this.dateCompleted = dateCompleted;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public User getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(User updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDate getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(LocalDate dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+}
---
+++ service-module/src/main/java/by/ceramogrand/processes/service/model/TaskPatternDTO.java
@@ -0,0 +1,116 @@
+package by.ceramogrand.processes.service.model;
+
+import by.ceramogrand.processes.repository.model.User;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TaskDTO {
+
+    private Long id;
+
+    private String name;
+    private String description;
+
+    private Boolean complete;
+    private UserDTO completedBy;
+    private LocalDateTime dateCompleted;
+
+    private Boolean deleted;
+
+    private UserDTO createdBy;
+    private LocalDate dateCreated;
+    private UserDTO updatedBy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getComplete() {
+        return complete;
+    }
+
+    public void setComplete(Boolean complete) {
+        this.complete = complete;
+    }
+
+    public UserDTO getCompletedBy() {
+        return completedBy;
+    }
+
+    public void setCompletedBy(UserDTO completedBy) {
+        this.completedBy = completedBy;
+    }
+
+    public LocalDateTime getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public void setDateCompleted(LocalDateTime dateCompleted) {
+        this.dateCompleted = dateCompleted;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public UserDTO getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(UserDTO createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public UserDTO getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(UserDTO updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDate getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(LocalDate dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    private LocalDate dateUpdated;
+
+
+}
